fea4d54e67c20238321b5ad4a8d02b0121b7299b
andreymalyshev-de <holyfather.2004@gmail.com>
2026-06-29 21:52:13 +0200

    Add: last-stone-weight - submission-6

---
+++ Data Structures & Algorithms/last-stone-weight/submission-6.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int max = 0;
+        for (int i: stones) {
+            if (i > max) max = i;
+        }
+
+        int[] nums = new int[max + 1];
+        for (int i: stones) {
+            nums[i]++;    
+        }
+
+        int a = -1;
+        int b = -1;
+        for (int i = max; i > 0; i--) {
+            while (nums[i] != 0) {
+                if (a < 0) {
+                    a = i;
+                    nums[i]--;
+                }
+                else if (b < 0) {
+                    b = i;
+                    nums[i]--;
+                }
+
+                if (a > 0 && b > 0) {
+                    nums[a - b]++;
+                    i = a;
+                    a = -1;
+                    b = -1;
+                }
+            }
+        }
+
+        return a < 0 ? 0 : a;
+    }
+}
