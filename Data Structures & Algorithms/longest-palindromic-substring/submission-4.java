4655c3f1f1d57f942ee510eb7c5f849b7a4769e9
andreymalyshev-de <holyfather.2004@gmail.com>
2026-07-20 09:28:24 +0200

    Add: longest-palindromic-substring - submission-4

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-4.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int l = 0;
+        int r = 1;
+        String str = s.substring(0, 1);
+
+        while (r < s.length()) {
+            if (isPal(s.substring(l, r + 1))) {
+                if (str.length() < r - l + 1) str = s.substring(l, r + 1);
+                if (l > 0 && r < s.length() - 1) {
+                    l--;
+                    r++;
+                }
+                else {
+                    if ((r - l + 1) % 2 == 0 && l > 0) l--;
+                    else { 
+                        l = (r + l) / 2;
+                        l++;
+                        r = l + 1;
+                    }
+                }
+            }
+            else if (r - l + 1 == 2 && l > 0) l--;
+            else {
+                l = (r + l) / 2;
+                l++;
+                r = l + 1;
+            }
+        }
+
+        return str;
+    }
+
+    private boolean isPal(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            r--;
+            l++;
+        }
+        return true;
+    }
+}
